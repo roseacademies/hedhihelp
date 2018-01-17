b3e5cda8f8cbfee1bdbfffbaaa5e89d073949ad9
sckp <32909879+sckp@users.noreply.github.com>
2018-01-16 17:47:18 -0800

    Changed onClick logic to sendMessage logic. Was having an error with @Override.

--- SDesignApp/app/src/main/java/com/example/sarahpagnani/sdesignapp/MainActivity.java
+++ SDesignApp/app/src/main/java/com/example/sarahpagnani/sdesignapp/MainActivity.java
@@ -13,13 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button0 = (Button) findViewById(R.id.loginbutton);
+//        Button button1 = (Button) findViewById(R.id.button3);
+//        button0.setOnClickListener((View.OnClickListener) this);
+//        button1.setOnClickListener((View.OnClickListener) this);
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, HomeActivity.class);
-        Button button = (Button) findViewById(R.id.loginbutton);
-        String message = button.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        switch (view.getId()) {
+            case R.id.loginbutton:
+                Intent intent0 = new Intent(this, HomeActivity.class);
+                Button button0 = (Button) findViewById(R.id.loginbutton);
+                String message0 = button0.getText().toString();
+                intent0.putExtra(EXTRA_MESSAGE, message0);
+                startActivity(intent0);
+                break;
+            case R.id.button3:
+                Intent intent1 = new Intent(this, AccountActivity.class);
+                Button button1 = (Button) findViewById(R.id.button3);
+                String message1 = button1.getText().toString();
+                intent1.putExtra(EXTRA_MESSAGE, message1);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+        }
     }
 }
